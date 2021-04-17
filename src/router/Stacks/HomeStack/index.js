ea5e209be98f562aef931199d9af23693b9a930a
Rachmizard <rachmizard11072000@gmail.com>
2021-04-18 00:51:14 +0700

    change transition homestack to sliderightfromIos

--- src/router/Stacks/HomeStack/index.js
+++ src/router/Stacks/HomeStack/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {createStackNavigator} from '@react-navigation/stack';
+import {createStackNavigator, TransitionPresets} from '@react-navigation/stack';
 import {DirectMessageScreen, HomeScreen} from '../../../screens';
 import {AppBarHeader} from '../../../components';
 
@@ -9,6 +9,7 @@ const HomeStack = () => (
   <Stack.Navigator
     screenOptions={{
       header: props => <AppBarHeader {...props} />,
+      ...TransitionPresets.SlideFromRightIOS,
     }}>
     <Stack.Screen name="Home" component={HomeScreen} />
     <Stack.Screen
